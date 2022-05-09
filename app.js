4e90ba804b1f890eca40989fbcccf74a91d634de
Tshepo Tsilo <tshepo9099@gmail.com>
2022-05-09 11:14:40 +0200

    add: progress bar style update on form step change

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const form_step_cards = document.querySelectorAll(".form__card");
 const progress_bar = document.querySelector(
   "[data-multistep-form-progress-bar]"
 );
-const progress_bar_icons = document.querySelectorAll("[data-step-icon]");
+const progress_bar_icons = multi_step_form.querySelectorAll(".progress__step");
 const submit_btn = document.querySelector("#msf-btn-submit");
+
+console.log("progress_bar_icons: ", progress_bar_icons);
 
 // Add click event listener to the multi-step form
 multi_step_form.addEventListener("click", (e) => {
   if (e.target.dataset?.action) {
     // Form button was clicked, handle form step actions
-    const currentStep = e.target.action;
+    console.log(e.target.dataset);
+    const currentStep = e.target.dataset.currentStep;
     if (e.target.dataset.action === "next") {
       // Show next form step
 
@@ -25,13 +28,26 @@ multi_step_form.addEventListener("click", (e) => {
       current_step_card.classList.toggle("form__previous-step");
 
       // Then show the next form step
-      const next_form_step_card = form_step_cards[e.target.dataset.currentStep];
+      const next_form_step_card = form_step_cards[currentStep];
       // Remove any reverse animations
       next_form_step_card.classList.remove("form__previous-step-reverse");
       next_form_step_card.classList.remove("form__active-step-reverse");
       // Toggle step change animations
       next_form_step_card.classList.toggle("form__active-step");
       next_form_step_card.classList.toggle("form__next-step");
+
+      // Update current step progress bar styles
+      multi_step_form
+        .querySelector(".progress__step-active")
+        .classList.add("progress__step-complete");
+      multi_step_form
+        .querySelector(".progress__step-active")
+        .classList.remove("progress__step-active");
+
+      progress_bar_icons[currentStep].classList.remove(
+        "progress__step-inactive"
+      );
+      progress_bar_icons[currentStep].classList.add("progress__step-active");
     } else if (e.target.dataset.action === "previous") {
       // Show previous form step
 
@@ -56,6 +72,17 @@ multi_step_form.addEventListener("click", (e) => {
       // Toggle step change animations
       previous_form_step_card.classList.toggle("form__active-step");
       previous_form_step_card.classList.toggle("form__previous-step");
+
+      // Update current step progress bar styles
+      multi_step_form
+        .querySelector(".progress__step-active")
+        .classList.add("progress__step-inactive");
+      multi_step_form
+        .querySelector(".progress__step-active")
+        .classList.remove("progress__step-active");
+      progress_bar_icons[previous_step_index - 1].classList.add(
+        "progress__step-active"
+      );
     } else if (e.target.dataset.action === "submit") {
       // Submit the form
     }

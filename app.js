b578eb878a33e9d66185ce1fe40970607ed342d3
Tshepo Tsilo <tshepo9099@gmail.com>
2022-05-09 09:08:32 +0200

    refactor: css animation and javascript toggle of css classes

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const progress_bar = document.querySelector(
 const progress_bar_icons = document.querySelectorAll("[data-step-icon]");
 const submit_btn = document.querySelector("#msf-btn-submit");
 
-console.log("form_step_cards: ", form_step_cards);
-
 // Add click event listener to the multi-step form
 multi_step_form.addEventListener("click", (e) => {
   if (e.target.dataset?.action) {
@@ -15,6 +13,18 @@ multi_step_form.addEventListener("click", (e) => {
     const currentStep = e.target.action;
     if (e.target.dataset.action === "next") {
       // Show next form step
+
+      // Start by hiding current form step
+      const current_step_card = [...form_step_cards].find((card) =>
+        card.classList.contains("form__active-step")
+      );
+      current_step_card.classList.toggle("form__active-step");
+      current_step_card.classList.toggle("form__previous-step");
+
+      // Then show the next form step
+      const next_form_step_card = form_step_cards[e.target.dataset.currentStep];
+      next_form_step_card.classList.toggle("form__active-step");
+      next_form_step_card.classList.toggle("form__next-step");
     } else if (e.target.dataset.action === "previous") {
       // Show previous form step
     } else if (e.target.dataset.action === "submit") {

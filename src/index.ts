2b9d0ca8374ee30830886d704ef9b1cd569ad68a
Tshepo Tsilo <tshepo9099@gmail.com>
2022-05-13 06:32:43 +0200

    added tsdx to project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { test } from './form';
+
+export const sum = (a: number, b: number) => {
+  if ('development' === process.env.NODE_ENV) {
+    console.log('boop');
+  }
+  return a + b;
+};
+
+export const hellowWorld = test;

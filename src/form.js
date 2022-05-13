328f7453821095a0bbfd6de90f8cc6073286a7c0
Tshepo Tsilo <tshepo9099@gmail.com>
2022-05-13 05:20:18 +0200

    fix: clean up form class

--- src/form.js
+++ src/form.js
@@ -1,7 +1,41 @@
 class Form {
+  formContainer; // HTML div that will contain the entire form container
+  initialState = {};
+  #formState;
   template;
 
-  constructor(options) {}
+  constructor(options) {
+    this.template = options.formTemplate;
+    // Loop throught all the inputTemplate and generate the initial form state
+    // for (let i = 1; i < this.template.length; i++) {
+    //   // Should return the initial form state in
+    // }
 
-  renderForm(template) {}
+    console.log(this.template);
+
+    this.template.map((step, stepIndex) => {
+      console.log("Step: ", step);
+      return step.inputsTemplate.map((input, inputIndex) => {
+        return (this.initialState[input.name] = {
+          elementId: input.name,
+          value: input?.defaultValue ? input?.defaultValue : "",
+          validation: {
+            isValid: true,
+          },
+          belongsToStep: step.stepElementId,
+          // orderInStep: input?.orderInStep ? input?.orderInStep : inputIndex,
+        });
+      });
+    });
+
+    console.log("Initial state: ", this.initialState);
+
+    this.renderForm(options);
+  }
+
+  renderForm(template) {
+    this.formContainer = console.log("Template: ", template);
+  }
 }
+
+export default Form;

20f6978fb7bad670d2ac0811cfbf30b59adb6215
Tshepo Tsilo <tshepo9099@gmail.com>
2022-05-14 01:44:17 +0200

    fix: Form object tests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
-import { test } from './form';
-
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+type InputTemplate = {
+  formElement: string;
+  type: string;
+  name: string;
+  placeholder: string;
+  defaultValue: string;
+  attributes: {
+    required: boolean;
+    readOnly: boolean;
+    min: 10;
+    max: 20;
+  };
+  validation: (value: any, formState: any) => boolean;
 };
 
-export const hellowWorld = test;
+type Options = {
+  appendTo: string;
+  formSteps: number;
+  formTemplate: {
+    stepElementId: string;
+    inputsTemplate: InputTemplate[];
+  }[];
+};
+
+class Form {
+  template;
+
+  constructor(options: Options) {
+    this.template = options.formTemplate;
+  }
+}
+
+export default Form;
